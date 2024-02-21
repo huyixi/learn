04aec3a57e7aff8bf056ffb578cc8751846a2f32
huyixi <hi@huyixi.com>
2024-02-21 09:39:10 +0800

    feat: Completed SwiftUI Tutorials - Creating and combining views

---
+++ AppleDevelop/SwiftUI/SwiftUI-Tutorials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 胡一希 on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,11 +1,34 @@
 use std::env;
 use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(&config.file_path)
+        .expect("Something went wrong reading the file");
+
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> Config {
+    if args.len() < 3 {
+        panic!("Not enough command-line arguments. Usage: <program> <query> <file_path>");
+    }
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {
+        query,
+        file_path,
+    }
 }
