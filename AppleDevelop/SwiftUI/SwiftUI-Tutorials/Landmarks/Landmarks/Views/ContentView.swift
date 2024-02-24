a619f5085e2f0584445f4650db7ece8cd975d828
huyixi <hi@huyixi.com>
2024-02-24 15:41:27 +0800

    feat: completed Landmark

---
+++ AppleDevelop/SwiftUI/SwiftUI-Tutorials/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 胡一希 on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
